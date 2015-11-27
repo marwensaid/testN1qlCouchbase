8fc8a04321a20c4d01e798d0dc59b63643d07b5c
marwensaid <marwensaidi1@gmail.com>
2015-11-27 16:19:44 +0100

    feat(spike N1ql couchbase javaC) add first query step --MS

--- src/main/java/QuerySpike.java
+++ src/main/java/QuerySpike.java
@@ -30,39 +30,37 @@ public class QuerySpike {
     }
 
     // obtenez une liste des espaces de noms à partir des métadonnées du système
-    public static List<N1qlQueryRow> getListOfNamespaces(Bucket b) {
-        return queryOnSystem(b, "namespaces");
+    public static List<N1qlQueryRow> getListOfNamespaces(Bucket bucket) {
+        return queryOnSystem(bucket, "namespaces");
     }
 
     // Obtenir une liste de banques de données à partir des métadonnées du système
-    public static List<N1qlQueryRow> getListOfDatastores(Bucket b) {
-        return queryOnSystem(b, "datastores");
+    public static List<N1qlQueryRow> getListOfDatastores(Bucket bucket) {
+        return queryOnSystem(bucket, "datastores");
     }
 
-    // Ceci est pour la recherche de certains types de métadonnées du système
-    // Telles que le système: les espaces de noms ou système: des banques de données
-    public static List<N1qlQueryRow> queryOnSystem(Bucket b, String subsystem) {
-        return queryToListOfQueryRow(b, "SELECT * FROM system:" + subsystem);
+    // Ceci est pour la recherche de certains types de métadonnées du système telles que le système:
+    // les espaces de noms ou système: des banques de données
+    public static List<N1qlQueryRow> queryOnSystem(Bucket bucket, String subsystem) {
+        return queryToListOfQueryRow(bucket, "SELECT * FROM system:" + subsystem);
     }
 
-    // Cette méthode suppose que les documents aériennes contiennent un certain
-    // Attribut, "type", et que la valeur de cet attribut est "airline".
-    public static List<N1qlQueryRow> getListOfAirlines(Bucket b) {
-        return getListByType(b, "airline");
+    // Cette méthode suppose que les documents aériennes contiennent un certain attribut, "type", et que la valeur de cet attribut est "airline".
+    public static List<N1qlQueryRow> getListOfAirlines(Bucket bucket) {
+        return getListByType(bucket, "airline");
     }
 
-    // Assuming the document contain an attribute called type, this lets
-    // you query the bucket for all documents of a certain type.
-    public static List<N1qlQueryRow> getListByType(Bucket b, String typeValue) {
-        return queryToListOfQueryRow(b, "SELECT * FROM `" + b.name() + "` where type=\"" + typeValue + "\";");
+    // En supposant que le document contient un attribut type dit, cela permet vous interrogez le seau pour tous les documents d'un certain type.
+    public static List<N1qlQueryRow> getListByType(Bucket bucket, String typeValue) {
+        return queryToListOfQueryRow(bucket, "SELECT * FROM `" + bucket.name() + "` where type=\"" + typeValue + "\";");
     }
 
-    // convenience method.  Given a bucket and a query string, which is assumed to be
-    // a SELECT, execute the query, print timing information, and return the results
-    // to the caller, not as a QueryResult, but as a list of QueryRows which the
-    // caller can iterate over and do things with.
-    public static List<N1qlQueryRow> queryToListOfQueryRow(Bucket b, String queryString) {
-        N1qlQueryResult queryResult = b.query(N1qlQuery.simple(queryString));
+    /** Méthode pratique. Compte tenu d'un seau et une chaîne de requête, qui est supposé être
+     SELECT, exécuter la requête, l'impression des informations de synchronisation, et retourner les résultats
+     À l'appelant, non pas comme un QueryResult, mais comme une liste de QueryRows laquelle le
+     Appelant peut parcourir et faire des choses avec.**/
+    public static List<N1qlQueryRow> queryToListOfQueryRow(Bucket bucket, String queryString) {
+        N1qlQueryResult queryResult = bucket.query(N1qlQuery.simple(queryString));
         List<N1qlQueryRow> rval = queryResult.allRows();
         N1qlMetrics qm = queryResult.info();
         System.out.println("The elapsed time of that query was:   " + qm.elapsedTime());
